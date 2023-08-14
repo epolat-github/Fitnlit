878b36cf0bd7726d11e751f0ed082414d5c65833
Erinç Polat <etuna1999@gmail.com>
2023-08-14 19:22:32 +0300

    Add navigation props and custom header

--- src/navigation/HomeStackNavigator.tsx
+++ src/navigation/HomeStackNavigator.tsx
@@ -1,8 +1,23 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RouteProp } from "@react-navigation/native";
+import {
+  NativeStackNavigationProp,
+  createNativeStackNavigator,
+} from "@react-navigation/native-stack";
 
+import HomeHeader from "../components/HomeHeader";
 import Home from "../screens/HomeScreens/Home";
 
-const HomeStack = createNativeStackNavigator();
+export type HomeStackParamList = {
+  Home: undefined;
+};
+
+export type HomeStackNavigationType<T extends keyof HomeStackParamList> =
+  NativeStackNavigationProp<HomeStackParamList, T>;
+
+export type HomeStackNavigationRouteProp<T extends keyof HomeStackParamList> =
+  RouteProp<HomeStackParamList, T>;
+
+const HomeStack = createNativeStackNavigator<HomeStackParamList>();
 
 const HomeStackNavigator = () => {
   return (
@@ -13,6 +28,7 @@ const HomeStackNavigator = () => {
           backgroundColor: "#fff",
         },
         headerShadowVisible: false,
+        header: (props) => <HomeHeader {...props} />,
       }}
     >
       <HomeStack.Screen name="Home" component={Home} />
