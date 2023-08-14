c0924414a96fa2bc1c0cb9056f1a81cce131b3f2
Erinç Polat <etuna1999@gmail.com>
2023-08-14 19:21:15 +0300

    Implement active workout card for home screen

---
+++ src/components/Home/ActiveWorkoutCard/index.tsx
@@ -0,0 +1,64 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { Text, View } from "react-native";
+
+import { colors, spacing } from "../../../theme";
+import Button from "../../Button";
+
+const ActiveWorkoutCard = () => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        paddingHorizontal: spacing.medium,
+        paddingVertical: spacing.medium * 1.5,
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderRadius: spacing.medium,
+        overflow: "hidden",
+      }}
+    >
+      <LinearGradient
+        colors={["#aa80ff", "#7733ff"]}
+        style={{
+          position: "absolute",
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0,
+        }}
+        start={{
+          x: 0.3,
+          y: 0.3,
+        }}
+        end={{
+          x: 1,
+          y: 1,
+        }}
+      />
+
+      <Text
+        style={{
+          color: "#fff",
+          fontWeight: "bold",
+        }}
+      >
+        Glute Burning Heavy Workout
+      </Text>
+
+      <Button
+        text="Continue"
+        containerStyle={{
+          backgroundColor: "#fff",
+          width: 100,
+          height: 35,
+          borderRadius: spacing.large,
+        }}
+        textStyle={{
+          color: colors.primary,
+        }}
+      />
+    </View>
+  );
+};
+
+export default ActiveWorkoutCard;
