77db39b051f52210950db7ddf04f3a8cc6036aa6
Erinç Polat <etuna1999@gmail.com>
2023-10-07 01:36:23 +0300

    Decrease mockup auth service timeouts

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -22,7 +22,7 @@ export const login = async (
         token:
           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
       });
-    }, 1000);
+    }, 400);
   });
 };
 
@@ -38,7 +38,7 @@ export const logout = async (): Promise<boolean> => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(true);
-    }, 1000);
+    }, 400);
   });
 };
 
