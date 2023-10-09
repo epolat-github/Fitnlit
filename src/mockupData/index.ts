89cac45a9b6bab8830e7c39d951b467e57386de8
Erinç Polat <etuna1999@gmail.com>
2023-10-09 03:48:01 +0300

    Create mockup data for weekly exercises

---
+++ src/mockupData/index.ts
@@ -0,0 +1,111 @@
+import { Exercise } from "../types/exercise.type";
+
+export const WEEKLY_PROGRAM: (Exercise[] | undefined)[] = [
+  [
+    {
+      id: "f8d0a688-0dc5-4d50-82dc-c609e88739c7",
+      name: "Dumbell Curl",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: false,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+    {
+      id: "17e3b188-9418-42a6-afd9-1cbd1552229f",
+      name: "Barbell Curl",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: true,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+    {
+      id: "a1d8fee0-5c9b-4cc1-b025-56e42379d570",
+      name: "Barbell Chest Press",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: false,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+  ],
+  [
+    {
+      id: "f8d0a688-0dc5-4d50-82dc-c609e88739c7",
+      name: "Dumbell Curl",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: false,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+    {
+      id: "17e3b188-9418-42a6-afd9-1cbd1552229f",
+      name: "Barbell Curl",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: true,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+    {
+      id: "a1d8fee0-5c9b-4cc1-b025-56e42379d570",
+      name: "Barbell Chest Press",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: false,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+  ],
+  [
+    {
+      id: "f8d0a688-0dc5-4d50-82dc-c609e88739c7",
+      name: "Dumbell Curl",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: false,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+    {
+      id: "17e3b188-9418-42a6-afd9-1cbd1552229f",
+      name: "Barbell Curl",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: true,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+    {
+      id: "a1d8fee0-5c9b-4cc1-b025-56e42379d570",
+      name: "Barbell Chest Press",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: false,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+  ],
+  undefined,
+  [
+    {
+      id: "f8d0a688-0dc5-4d50-82dc-c609e88739c7",
+      name: "Dumbell Curl",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: false,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+    {
+      id: "17e3b188-9418-42a6-afd9-1cbd1552229f",
+      name: "Barbell Curl",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: true,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+    {
+      id: "a1d8fee0-5c9b-4cc1-b025-56e42379d570",
+      name: "Barbell Chest Press",
+      description:
+        "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+      isCompleted: false,
+      equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    },
+  ],
+  undefined,
+  undefined,
+];
