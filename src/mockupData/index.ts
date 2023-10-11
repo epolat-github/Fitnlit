31e20dc4f81bdb4533fb74cf5637b8b094c70b57
Erinç Polat <etuna1999@gmail.com>
2023-10-11 03:09:05 +0300

    Add new mockup data

--- src/mockupData/index.ts
+++ src/mockupData/index.ts
@@ -109,3 +109,30 @@ export const WEEKLY_PROGRAM: (Exercise[] | undefined)[] = [
   undefined,
   undefined,
 ];
+
+export const ALL_EXERCISES = [
+  {
+    id: "f8d0a688-0dc5-4d50-82dc-c609e88739c7",
+    name: "Dumbell Curl",
+    description:
+      "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+    isCompleted: false,
+    equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+  },
+  {
+    id: "17e3b188-9418-42a6-afd9-1cbd1552229f",
+    name: "Barbell Curl",
+    description:
+      "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+    isCompleted: true,
+    equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+  },
+  {
+    id: "a1d8fee0-5c9b-4cc1-b025-56e42379d570",
+    name: "Barbell Chest Press",
+    description:
+      "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+    isCompleted: false,
+    equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+  },
+];
