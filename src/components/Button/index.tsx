a74a67bf39bdb551b803c9e261e2fec4884550b2
Erinç Polat <etuna1999@gmail.com>
2023-10-17 00:09:16 +0300

    Add disabled state and make background animated of custom button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,13 @@ import {
   PressableProps,
   Text,
   TextStyle,
-  StyleProp,
   ViewStyle,
 } from "react-native";
 import Animated, {
+  Easing,
+  interpolateColor,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
@@ -18,14 +20,20 @@ const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
 interface ButtonProps extends PressableProps {
   textStyle?: TextStyle;
-  containerStyle?: StyleProp<ViewStyle>;
+  containerStyle?: ViewStyle;
   text: string;
+  disabled?: boolean;
 }
 
 const Button = (props: ButtonProps) => {
-  const { containerStyle, textStyle, text, ...rest } = props;
+  const { containerStyle, textStyle, text, disabled, ...rest } = props;
 
   const scale = useSharedValue(1);
+  const backgroundColor = useDerivedValue(() => {
+    return disabled
+      ? withTiming(0, { duration: 300, easing: Easing.inOut(Easing.ease) })
+      : withTiming(1, { duration: 300, easing: Easing.inOut(Easing.ease) });
+  });
 
   const onPressIn = () => {
     scale.value = withTiming(0.95);
@@ -37,6 +45,12 @@ const Button = (props: ButtonProps) => {
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
+      backgroundColor: interpolateColor(
+        backgroundColor.value,
+        [0, 1],
+        ["gray", containerStyle?.backgroundColor?.toString() ?? colors.primary],
+      ),
+
       transform: [
         {
           scale: scale.value,
@@ -49,9 +63,9 @@ const Button = (props: ButtonProps) => {
     <AnimatedPressable
       onPressIn={onPressIn}
       onPressOut={onPressOut}
+      disabled={disabled}
       style={[
         {
-          backgroundColor: colors.primary,
           height: 50,
           borderRadius: spacing.medium,
           width: "100%",
