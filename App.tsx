3e4440b12e66904001b9dce70002b016a2a69036
Erinç Polat <etuna1999@gmail.com>
2024-01-09 00:53:25 +0300

    Add snackbar feature

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { PortalProvider } from "@gorhom/portal";
 import * as SplashScreen from "expo-splash-screen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider as ReduxProvider } from "react-redux";
 
+import { SnackbarContextProvider } from "./src/context/SnackbarContext";
 import MainNavigationContainer from "./src/navigation";
 import { store } from "./src/utils/store";
 
@@ -11,14 +13,18 @@ SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <BottomSheetModalProvider>
-        <SafeAreaProvider>
-          <ReduxProvider store={store}>
-            <MainNavigationContainer />
-          </ReduxProvider>
-        </SafeAreaProvider>
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
+    <PortalProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SnackbarContextProvider>
+          <BottomSheetModalProvider>
+            <SafeAreaProvider>
+              <ReduxProvider store={store}>
+                <MainNavigationContainer />
+              </ReduxProvider>
+            </SafeAreaProvider>
+          </BottomSheetModalProvider>
+        </SnackbarContextProvider>
+      </GestureHandlerRootView>
+    </PortalProvider>
   );
 }
