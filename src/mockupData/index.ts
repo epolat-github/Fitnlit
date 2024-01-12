477e97eee538b2e42532ae6800242c84dbf6a424
Erinç Polat <etuna1999@gmail.com>
2024-01-12 18:05:33 +0300

    Add mockup workout data

--- src/mockupData/index.ts
+++ src/mockupData/index.ts
@@ -2,6 +2,36 @@ import { colors } from "../theme";
 import { Exercise } from "../types/exercise.type";
 import { Meal, WeeklyMealProgram } from "../types/meals.type";
 
+export const WORKOUT = [
+  {
+    id: "f8d0a688-0dc5-4d50-82dc-c609e88739c7",
+    name: "Dumbell Curl",
+    description:
+      "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+    isCompleted: false,
+    equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    rest: 60,
+  },
+  {
+    id: "17e3b188-9418-42a6-afd9-1cbd1552229f",
+    name: "Barbell Curl",
+    description:
+      "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+    isCompleted: true,
+    equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    rest: 60,
+  },
+  {
+    id: "a1d8fee0-5c9b-4cc1-b025-56e42379d570",
+    name: "Barbell Chest Press",
+    description:
+      "Eu proident velit esse ea qui commodo est dolor reprehenderit nisi veniam do aliqua.",
+    isCompleted: false,
+    equipments: ["Foam Roller", "Yoga Mat", "Booty Band", "Bench"],
+    rest: 60,
+  },
+];
+
 export const WEEKLY_PROGRAM: (Exercise[] | undefined)[] = [
   [
     {
