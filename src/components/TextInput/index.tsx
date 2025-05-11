29b747546d117b148eb0da60a19573577c4f3792
Erinç Polat <51881836+epolat-github@users.noreply.github.com>
2025-05-11 03:04:17 +0300

    Add error and right icon support to the custom Text Input

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,36 +1,63 @@
-import { ReactNode, forwardRef } from "react";
-import { TextInput as BaseTextInput, TextInputProps } from "react-native";
+import { forwardRef } from "react";
+import {
+  TextInput as BaseTextInput,
+  StyleSheet,
+  TextInputProps as BaseTextInputProps,
+  View,
+  Text,
+} from "react-native";
 
 import { spacing } from "../../theme";
 
-interface Props {
-  children?: ReactNode;
-  type: "submit" | "button";
+interface TextInputProps extends BaseTextInputProps {
+  rightIcon?: React.ReactNode | (() => React.ReactNode);
+  error?: string;
 }
+
 export type TextInputRef = BaseTextInput;
 
 const TextInput = forwardRef<TextInputRef, TextInputProps>((props, ref) => {
-  const { style, ...restProps } = props;
+  const { style, rightIcon, error, ...restProps } = props;
 
   return (
-    <BaseTextInput
-      ref={ref}
-      autoCapitalize="none"
-      autoCorrect={false}
-      style={[
-        {
-          backgroundColor: "#F7F8FA",
-          height: 55,
-          borderColor: "#EAECEF",
-          borderWidth: 1,
-          borderRadius: spacing.medium,
-          paddingHorizontal: spacing.medium,
-        },
-        style,
-      ]}
-      {...restProps}
-    />
+    <View style={styles.container}>
+      <View style={styles.textInputContainer}>
+        <BaseTextInput
+          ref={ref}
+          style={[styles.input, style]}
+          autoCorrect={false}
+          {...restProps}
+        />
+        {typeof rightIcon === "function" ? rightIcon() : rightIcon}
+      </View>
+      {error && <Text style={styles.errorText}>{error}</Text>}
+    </View>
   );
 });
 
 export default TextInput;
+
+const styles = StyleSheet.create({
+  container: {
+    gap: spacing.tiny,
+    height: 70,
+  },
+  textInputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#F7F8FA",
+    borderColor: "#EAECEF",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 12,
+    height: 55,
+  },
+  input: {
+    flex: 1,
+    paddingRight: 10,
+  },
+  errorText: {
+    color: "tomato",
+    paddingLeft: 5,
+  },
+});
