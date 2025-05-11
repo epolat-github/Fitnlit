b6cd93cc2a40f34afeebfab19a4789baeb5bfe84
Erinç Polat <51881836+epolat-github@users.noreply.github.com>
2025-05-11 22:27:16 +0300

    remove portal

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import { PortalProvider } from "@gorhom/portal";
 import * as SplashScreen from "expo-splash-screen";
+import moment from "moment";
+import "moment/locale/tr";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider as ReduxProvider } from "react-redux";
@@ -9,22 +10,22 @@ import { SnackbarContextProvider } from "./src/context/SnackbarContext";
 import MainNavigationContainer from "./src/navigation";
 import { store } from "./src/utils/store";
 
+moment.locale("tr");
+
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <PortalProvider>
-        <BottomSheetModalProvider>
-          <SnackbarContextProvider>
-            <SafeAreaProvider>
-              <ReduxProvider store={store}>
-                <MainNavigationContainer />
-              </ReduxProvider>
-            </SafeAreaProvider>
-          </SnackbarContextProvider>
-        </BottomSheetModalProvider>
-      </PortalProvider>
+      <BottomSheetModalProvider>
+        <SnackbarContextProvider>
+          <SafeAreaProvider>
+            <ReduxProvider store={store}>
+              <MainNavigationContainer />
+            </ReduxProvider>
+          </SafeAreaProvider>
+        </SnackbarContextProvider>
+      </BottomSheetModalProvider>
     </GestureHandlerRootView>
   );
 }
